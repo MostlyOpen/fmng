4f5bcfc6c10c7140841e4663fe21998cb7c57271
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-04-27 21:25:19 -0300

    Updated file "data/fmng.py".

--- data/fmng.py
+++ data/fmng.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ###############################################################################
-#                                                                             #
-# Copyright (C) 2016-Today  Carlos Eduardo Vercelino - CLVsol                 #
-#                                                                             #
-# This program is free software: you can redistribute it and/or modify        #
-# it under the terms of the GNU Affero General Public License as published by #
-# the Free Software Foundation, either version 3 of the License, or           #
-# (at your option) any later version.                                         #
-#                                                                             #
-# This program is distributed in the hope that it will be useful,             #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
-# GNU Affero General Public License for more details.                         #
-#                                                                             #
-# You should have received a copy of the GNU Affero General Public License    #
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 ###############################################################################
 
 from __future__ import print_function
@@ -65,7 +66,7 @@ def get_arguments():
 
 def secondsToStr(t):
 
-    return "%d:%02d:%02d.%03d" % reduce(lambda ll, b: divmod(ll[0], b) + ll[1:], [(t*1000,), 1000, 60, 60])
+    return "%d:%02d:%02d.%03d" % reduce(lambda ll, b: divmod(ll[0], b) + ll[1:], [(t * 1000,), 1000, 60, 60])
 
 
 if __name__ == '__main__':
@@ -91,6 +92,20 @@ if __name__ == '__main__':
     start = time()
 
     client = erppeek.Client(server, dbname, username, password)
+
+    # file_args = []
+    # db_path = 'data/fmng.sqlite'
+    # print('-->', client, file_args, db_path)
+    # print('--> Executing fmng_entity_export_sqlite()...')
+    # print()
+    # fmng_entity_export_sqlite(client, file_args, db_path)
+
+    # file_args = []
+    # db_path = 'data/fmng.sqlite'
+    # print('-->', client, file_args, db_path)
+    # print('--> Executing fmng_entity_import_sqlite()...')
+    # print()
+    # fmng_entity_import_sqlite(client, file_args, db_path)
 
     # tag_args = []
     # db_path = 'data/fmng.sqlite'
